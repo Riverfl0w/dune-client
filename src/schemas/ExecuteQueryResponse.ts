f2712efb2285f9f59d7999ae4b6cecda8e64f1bc
Mathieu Bour <mathieu@riverflow.co>
2024-02-22 09:56:58 +0100

    fix: allow QUERY_STATE_EXECUTING in query.execute

--- src/schemas/ExecuteQueryResponse.ts
+++ src/schemas/ExecuteQueryResponse.ts
@@ -1,9 +1,19 @@
 import { z } from 'zod';
 
-const ExecuteQueryResponse = z.object({
-  execution_id: z.string(),
-  state: z.literal('QUERY_STATE_PENDING'),
-});
+const ExecuteQueryResponse = z
+  .object({
+    execution_id: z.string(),
+  })
+  .and(
+    z.discriminatedUnion('state', [
+      z.object({
+        state: z.literal('QUERY_STATE_PENDING'),
+      }),
+      z.object({
+        state: z.literal('QUERY_STATE_EXECUTING'),
+      }),
+    ]),
+  );
 type ExecuteQueryResponse = z.infer<typeof ExecuteQueryResponse>;
 
 export default ExecuteQueryResponse;
